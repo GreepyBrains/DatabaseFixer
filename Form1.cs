d2aca188dcdaee08e093ed0bee832c31ff29a672
greepybrains <greepybrains@gmail.com>
2024-05-25 09:02:54 +0200

    Added functionality for: replace,groupby/orderby, xtime to gfostest, || to +

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Globalization;
+
 namespace DatabaseFixer
 {
     public partial class Form1 : Form
@@ -13,7 +16,25 @@ namespace DatabaseFixer
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox2.Text = textBox1.Text;
+            string _databaseQuery = textBox1.Text;
+
+            _databaseQuery = _databaseQuery.Replace("or replace", "", true, CultureInfo.InvariantCulture);
+            _databaseQuery = _databaseQuery.Replace("||", "+", true, CultureInfo.InvariantCulture);
+            _databaseQuery = _databaseQuery.Replace("XTIME", "gfostest", true, CultureInfo.InvariantCulture);
+
+            //Deletion of Group By
+            string[] _queryLines = _databaseQuery.Split(new string[] { Environment.NewLine}, StringSplitOptions.None);
+            _databaseQuery = "";
+            for (int i = 0; i < _queryLines.Length; i++)
+            {
+                if (_queryLines[i].ToLower().Contains("group by") || _queryLines[i].ToLower().Contains("order by"))
+                    _queryLines[i] = "";
+
+                Debug.WriteLine(i);
+                _databaseQuery += _queryLines[i];
+                _databaseQuery += Environment.NewLine;
+            }
+            textBox2.Text = _databaseQuery;
         }
     }
 }
